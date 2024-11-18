4052d25b1f79ff3f0ef7be23946e2e6909abc17a
Sheena Gonzalez <shngonzalez@gmail.com>
2024-11-17 18:25:11 -0800

    Replace function declarations with function expressions and add setDisplay function

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const add = function (a, b) {
+const add = function addTwoNumbers(a, b) {
   return a + b;
 }
 
-const subtract = function (a, b) {
+const subtract = function subtractTwoNumbers(a, b) {
   return a - b;
 }
 
-const multiply = function (a, b) {
+const multiply = function multiplyTwoNumbers(a, b) {
   return a * b;
 }
 
-const divide = function (a, b) {
+const divide = function divideTwoNumbers(a, b) {
   return a / b;
 }
 
 let numA, operator, numB;
 
-const operate = function (numA, operator, numB) {
+const operate = function operateOnTwoNumbers(numA, operator, numB) {
   switch (operator) {
     case operator === '+':
       return add(numA, numB);
@@ -28,3 +28,35 @@ const operate = function (numA, operator, numB) {
       return divide(numA, numB);
   }
 }
+
+const keyButtons = [...(document.querySelectorAll('.key'))];
+const display = document.querySelector('.display-input');
+const digits = '0123456789';
+const operators = '+-*/';
+let currNumber = '';
+
+const setDisplay = function setInputToDisplay(key) {
+  if (digits.includes(key) || (key === '.' && !currNumber.includes('.'))) {
+    currNumber += key;
+    display.value += key;
+  } else if (operators.includes(key)) {
+    currNumber = '';
+    display.value += key;
+  } else if (key === 'Backspace') {
+    display.value = display.value.slice(0, -1);
+  } else if (key === 'Escape') {
+    display.value = null;
+  }
+}
+
+keyButtons.map((keyBtn) => {
+  keyBtn.addEventListener('click', (e) => { 
+    const key = e.target.getAttribute('data-key');
+    setDisplay(key);
+  })
+})
+
+window.addEventListener('keydown', function (e) {
+  const key = document.querySelector(`.key[data-key='${e.key}']`);
+  setDisplay(key);
+});
