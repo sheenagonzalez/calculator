f2f3152f06626e51dc91955ec444e74ebf3aebe0
Sheena Gonzalez <shngonzalez@gmail.com>
2024-11-17 16:50:43 -0800

    Create number and operator variables and operate function

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
   return a / b;
 }
+
+let numA, operator, numB;
+
+const operate = function (numA, operator, numB) {
+  switch (operator) {
+    case operator === '+':
+      return add(numA, numB);
+    case operator === '-':
+      return subtract(numA, numB);
+    case operator === '*':
+      return multiply(numA, numB);
+    case operator === '/':
+      return divide(numA, numB);
+  }
+}
